6117084bb344890d79de750ff21fa85a8d866e02
mahadi-hsn <mahadi2389@gmail.com>
2026-05-16 17:32:31 +0600

    Proceed to checkout functionality added

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,11 +1,15 @@
 import CartItems from "./CartItems";
 
 const Cart = ({ cart, setCart }) => {
-    console.log(cart);
+  console.log(cart);
   const totalPrice = cart.reduce(
     (total, item) => total + Number(item.price || 0),
-    0
+    0,
   );
+
+  const handleProceedToCheckout = () =>{
+    setCart([]);
+  }
 
   return (
     <div className="border rounded-lg p-5">
@@ -22,7 +26,12 @@ const Cart = ({ cart, setCart }) => {
         <h1>Total:</h1>
         <p>${totalPrice}</p>
       </div>
-      <button className="btn w-full bg-linear-to-r from-[#5f2df5] to-[#a10df5] shadow-[0_10px_25px_rgba(106,44,240,0.35) text-white rounded-full">Proceed to Checkout</button>
+      <button
+        onClick={handleProceedToCheckout}
+        className="btn w-full bg-linear-to-r from-[#5f2df5] to-[#a10df5] shadow-[0_10px_25px_rgba(106,44,240,0.35) text-white rounded-full"
+      >
+        Proceed to Checkout
+      </button>
     </div>
   );
 };
